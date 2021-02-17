88409b5131674583632a7bd42d9508deda38ba52
Cristóbal Valverde Albo <1251881+valverdealbo@users.noreply.github.com>
2021-02-17 20:47:59 +0100

    fix: fix random date-time timezone

--- src/date-time-string-range.ts
+++ src/date-time-string-range.ts
@@ -1,6 +1,5 @@
 import type { FromSchema } from 'json-schema-to-ts';
 import Chance from 'chance';
-import formatRFC3339 from 'date-fns/formatRFC3339';
 import addDays from 'date-fns/addDays';
 import { datetimeString } from './date-time-string';
 
@@ -13,5 +12,5 @@ export type DatetimeStringRange = FromSchema<typeof datetimeStringRange>;
 export function randomDatetimeStringRange(): DatetimeStringRange {
   const from = chance.date();
   const to = addDays(from, chance.integer({ min: 1, max: 365 }));
-  return [formatRFC3339(from, { fractionDigits: 3 }), formatRFC3339(to, { fractionDigits: 3 })];
+  return [from.toISOString(), to.toISOString()];
 }
